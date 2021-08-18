52f3a7e12dda15235ef42ed672f497fce5ec480a
RDPrangon12 <ranatdasprangon@gmail.com>
2021-08-19 03:46:32 +0600

    Footer Solved

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,10 @@
 /** @type {import('@sveltejs/kit').Config} */
-import vercel from '@sveltejs/adapter-vercel';
+//import vercel from '@sveltejs/adapter-vercel';
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
-		adapter: vercel()
+		target: '#svelte'
+		//adapter: vercel()
 	}
 };
 
